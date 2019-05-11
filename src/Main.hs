7aa99e0e866833960fb9f3a8aa9f1a4cccaf23f0
adithyaov <adi.obilisetty@gmail.com>
2019-05-12 00:19:01 +0530

    Completed Project, Made command line tool

--- src/Main.hs
+++ src/Main.hs
@@ -5,16 +5,33 @@ import IOJson
 import Data.Aeson
 import Data.Map ((!))
 import qualified Data.Map as Map
+import qualified Data.ByteString.Lazy as BSL
+import System.Environment
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  case args of
+    (p:s:o:_) -> parseO o p s
+    _ -> print "3 args required (min), src file, dest file and option."
+  where
+    parseO o p s
+      | o == "STEPS" = readWFile STEPS p s
+      | o == "FINAL" = readWFile FINAL p s
+      | otherwise = print "Option should either be STEPS for steps or FINAL for final"
 
-readWFile :: FilePath -> IO ()
-readWFile p = do
+data Option = FINAL | STEPS
+
+readWFile :: Option -> FilePath -> FilePath -> IO ()
+readWFile o p s = do
   mRW <- decodeFileStrict p
   case (mRW :: Maybe FileSig) of
     Nothing -> print "Incorrect Format"
     Just rW -> do
       let (w, r) = makeWorld rW
       let wF = fixedPointCalc r w
-      print $ totalGiven "Palolene" wF 
+      let steps = Map.map complete wF
+      let final = Map.map foldComplete steps
+      case o of
+        FINAL -> BSL.writeFile s (encode final)
+        STEPS -> BSL.writeFile s (encode steps)
